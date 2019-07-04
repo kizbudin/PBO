6c9173a19ad566aaf890588369fd63fd08072d80
fallcrescent <mkizbudin@gmail.com>
2019-07-04 22:36:51 +0700

    update

---
+++ src/tugaspbo/TugasPbo.java
@@ -0,0 +1,15 @@
+
+package tugaspbo;
+
+
+public class TugasPbo {
+
+    public static void main(String[] args) {
+       
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
